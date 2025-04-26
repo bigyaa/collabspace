97a38179870052495b2b52e2286305500d62855d
Bigya <bigyabajracharya@gmail.com>
2025-04-26 01:30:52 -0400

    feat: setup Next.js frontend with TailwindCSS

---
+++ client/src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">Register</h1>
+      </div>
+    );
+  }
